9a57124949a23a3d323c2d433a1d54f8ce89e977
Naitik1126 <npurshot@gitam.in>
2025-07-14 15:13:19 +0530

    Add files via upload

---
+++ hard coding variables.py
@@ -0,0 +1,3 @@
+a, b, c, t = -0.5, 4, 10, 5
+T = a * t**2 + b * t + c
+print(f"Temperature at t={t}: {T:.2f}")
---
+++ keyboard input.py
@@ -0,0 +1,6 @@
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+t = float(input("Enter time t: "))
+T = a * t**2 + b * t + c
+print(f"Temperature at t={t}: {T:.2f}")
